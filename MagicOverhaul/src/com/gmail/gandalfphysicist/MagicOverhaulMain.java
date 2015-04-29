84702741eaf0567ae8b86c7520e94119a64b555f
jakeyray18 <jakeyray18@gmail.com>
2015-04-29 16:56:09 -0500

    Sorted onDisable

--- MagicOverhaul/src/com/gmail/gandalfphysicist/MagicOverhaulMain.java
+++ MagicOverhaul/src/com/gmail/gandalfphysicist/MagicOverhaulMain.java
@@ -2,6 +2,8 @@ package com.gmail.gandalfphysicist;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -20,10 +22,15 @@ public class MagicOverhaulMain extends JavaPlugin {
 
         ShapelessRecipe MAP = new ShapelessRecipe(ItemStacks.map()).addIngredient(Material.BOWL).addIngredient(Material.STICK);
         getServer().addRecipe(MAP);
+
     }
 
     public void onDisable() {
 
+        //Unregister Recipes
+        getServer().clearRecipes();
+
+        //Log Plugin Disabled
         getLogger().log(Level.INFO, ChatColor.DARK_RED + "Plugin Disabled.");
 
     }
