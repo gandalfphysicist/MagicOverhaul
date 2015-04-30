afa2672a50c96e944721a23d40b788abf0aede76
jakeyray18 <jakeyray18@gmail.com>
2015-04-29 20:09:21 -0500

    Disabled Enchanting table recipe.

--- MagicOverhaul/src/com/gmail/gandalfphysicist/MagicOverhaulMain.java
+++ MagicOverhaul/src/com/gmail/gandalfphysicist/MagicOverhaulMain.java
@@ -4,9 +4,11 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
 import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Iterator;
 import java.util.logging.Level;
 
 public class MagicOverhaulMain extends JavaPlugin {
@@ -22,6 +24,16 @@ public class MagicOverhaulMain extends JavaPlugin {
 
         ShapelessRecipe MAP = new ShapelessRecipe(ItemStacks.map()).addIngredient(Material.BOWL).addIngredient(Material.STICK);
         getServer().addRecipe(MAP);
+
+        //Disable Enchanting Table Recipe
+        Iterator<Recipe> it = getServer().recipeIterator();
+        Recipe recipe;
+        while(it.hasNext()) {
+            recipe = it.next();
+            if (recipe != null && recipe.getResult().getType().equals(Material.ENCHANTMENT_TABLE)) {
+                it.remove();
+            }
+        }
 
     }
 
