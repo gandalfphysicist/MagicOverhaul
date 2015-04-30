d860364d7a966e212a9982991ee5548c1277fc93
jakeyray18 <jakeyray18@gmail.com>
2015-04-29 20:55:04 -0500

    Created Class to disable recipes.

---
+++ MagicOverhaul/src/com/gmail/gandalfphysicist/Utils/RemoveRecipes.java
@@ -0,0 +1,37 @@
+package com.gmail.gandalfphysicist.Utils;
+
+import com.gmail.gandalfphysicist.MagicOverhaulMain;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+
+import java.util.Iterator;
+
+public class RemoveRecipes {
+
+    MagicOverhaulMain plugin;
+
+    public RemoveRecipes(MagicOverhaulMain instance) {
+        plugin = instance;
+    }
+
+    public static void removeRecipes(Material mat, int durability) {
+        Iterator<Recipe> it = Bukkit.getServer().recipeIterator();
+        Recipe recipe;
+        ItemStack result;
+        Material resultType;
+        while (it.hasNext()) {
+            recipe = it.next();
+            if (recipe != null) {
+                result = recipe.getResult();
+                resultType = result.getType();
+                if (resultType.equals(mat) && result.getDurability() == durability) {
+                    it.remove();
+                }
+
+            }
+        }
+    }
+}
+
