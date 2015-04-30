e18da8edf6675de1ef8baa8d55d8b842a806d6f8
BuildTools <gandalfphysicist@gmail.com>
2015-04-30 17:32:54 -0500

    Salve Crafting Mk I

---
+++ MagicOverhaul/src/com/gmail/gandalfphysicist/SalveCraftingListener.java
@@ -0,0 +1,31 @@
+package com.gmail.gandalfphysicist;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class SalveCraftingListener implements Listener {
+	
+	@EventHandler
+	public void onInventoryClickEvent(InventoryClickEvent event) {
+		
+		Inventory inventory = event.getInventory();
+		ItemStack item = event.getCurrentItem();
+		ItemStack cursor = event.getCursor();
+		//int cursoramount = cursor.getAmount();
+		
+		if(inventory.getName().contains(ChatColor.DARK_AQUA + "Salve Crafting")) {
+			if(item != null) {
+				if(item.equals(ItemStacks.glass())) {
+					if(cursor.getType() != Material.AIR) {
+						cursor.setType(Material.WATER);
+					}
+				}
+			}
+		}
+	}
+}
