59f0691914d42e9482c2d5fef1ed636fa1f04aa8
jakeyray18 <jakeyray18@gmail.com>
2015-04-30 19:19:20 -0500

    Set Cursor to null

--- MagicOverhaul/src/com/gmail/gandalfphysicist/SalveCraftingListener.java
+++ MagicOverhaul/src/com/gmail/gandalfphysicist/SalveCraftingListener.java
@@ -9,23 +9,22 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class SalveCraftingListener implements Listener {
-	
-	@EventHandler
-	public void onInventoryClickEvent(InventoryClickEvent event) {
-		
-		Inventory inventory = event.getInventory();
-		ItemStack item = event.getCurrentItem();
-		ItemStack cursor = event.getCursor();
-		//int cursoramount = cursor.getAmount();
-		
-		if(inventory.getName().contains(ChatColor.DARK_AQUA + "Salve Crafting")) {
-			if(item != null) {
-				if(item.equals(ItemStacks.glass())) {
-					if(cursor.getType() != Material.AIR) {
-						cursor.setType(Material.WATER);
-					}
-				}
-			}
-		}
-	}
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event) {
+
+        Inventory inventory = event.getInventory();
+        ItemStack item = event.getCurrentItem();
+        ItemStack cursor = event.getCursor();
+
+        if (inventory.getName().contains(ChatColor.DARK_AQUA + "Salve Crafting")) {
+            if (item != null) {
+                if (item.equals(ItemStacks.glass())) {
+                    if (cursor.getType() != Material.AIR) {
+                        event.getView().setCursor(null);
+                    }
+                }
+            }
+        }
+    }
 }
