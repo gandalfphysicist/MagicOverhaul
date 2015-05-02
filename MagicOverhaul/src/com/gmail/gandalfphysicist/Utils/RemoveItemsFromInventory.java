42ad5d77fd23e11444da2b432c547edfc5f8e0d5
jakeyray18 <jakeyray18@gmail.com>
2015-05-02 00:06:33 -0500

    Added damage value suppott to remove item class.

--- MagicOverhaul/src/com/gmail/gandalfphysicist/Utils/RemoveItemsFromInventory.java
+++ MagicOverhaul/src/com/gmail/gandalfphysicist/Utils/RemoveItemsFromInventory.java
@@ -1,25 +1,28 @@
 package com.gmail.gandalfphysicist.Utils;
 
 import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 public class RemoveItemsFromInventory {
 
-    public static void removeInventoryItems(PlayerInventory inv, Material type, int amount) {
-        for (ItemStack is : inv.getContents()) {
-            if (is != null && is.getType() == type) {
+    public static void removeInventoryItems(PlayerInventory inv, Material type, int amount, short damage) {
+        ItemStack[] items = inv.getContents();
+        for (int i = 0; i < items.length; i++) {
+            ItemStack is = items[i];
+            if (is != null && is.getType() == type && is.getDurability() == damage) {
                 int newamount = is.getAmount() - amount;
                 if (newamount > 0) {
                     is.setAmount(newamount);
                     break;
                 } else {
-                    inv.remove(is);
+                    items[i] = new ItemStack(Material.AIR);
                     amount = -newamount;
                     if (amount == 0) break;
                 }
             }
         }
+        inv.setContents(items);
     }
-
 }
